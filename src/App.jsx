1ed000825c6548d0b0031d34fa733649c49b6534
ahmed <ay747790@gmail.com>
2025-06-01 18:27:58 +0300

    add daisyui and redux basic implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { Provider } from "react-redux";
 
 import Layout from "./components/Layout";
+import store from "./features/store";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Layout />,
-      children:[]
+      children: [],
     },
   ]);
 
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+  );
 }
 
 export default App;
