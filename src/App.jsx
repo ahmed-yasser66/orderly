2787ab0b69fa86272216ab96c8baeabdfef15edd
Ahmed-Bakr611 <ahmedhomes611@gmail.com>
2025-06-03 05:07:59 +0300

    after merge with yasser , add api gateway to talk with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -35,29 +35,28 @@ function App() {
   ]);
 
   return (
-    // <Provider store={store}>
-    //   <RouterProvider router={router}></RouterProvider>
-      
-    // </Provider>
-    <>
-      <Navbar />
-  <div className="container mx-auto p-15 space-x-6">
-    <Button variant="primary">Primary Button</Button>
-    <Button variant="secondary">Secondary Button</Button>
-    <Button variant="success">Success Button</Button>
-    <Button variant="warning">Warning Button</Button>
-    <Button variant="info">Info Button</Button>
-      </div>
-      <Table
-        headers={["Amount", "Item", "Price", "Total"]}
-        data={[
-          [2, "Foul", 6, "12$"],
-          [1, "Ta3meya", 10, "10$"],
-          [3, "Eggs", 5, "15$"],
-          [1, "Cheese", 12, "12$"],
-        ]}
-      />
-    </>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
+    //   <>
+    //     <Navbar />
+    // <div className="container mx-auto p-15 space-x-6">
+    //   <Button variant="primary">Primary Button</Button>
+    //   <Button variant="secondary">Secondary Button</Button>
+    //   <Button variant="success">Success Button</Button>
+    //   <Button variant="warning">Warning Button</Button>
+    //   <Button variant="info">Info Button</Button>
+    //     </div>
+    //     <Table
+    //       headers={["Amount", "Item", "Price", "Total"]}
+    //       data={[
+    //         [2, "Foul", 6, "12$"],
+    //         [1, "Ta3meya", 10, "10$"],
+    //         [3, "Eggs", 5, "15$"],
+    //         [1, "Cheese", 12, "12$"],
+    //       ]}
+    //     />
+    //   </>
   );
 }
 
