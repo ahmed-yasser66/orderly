fe23f36eac4485d756e7ed28e973aa601aba292d
ahmed <ay747790@gmail.com>
2025-06-03 16:31:01 +0300

    landing page done :)

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,9 @@ import { Provider } from "react-redux";
 
 import Layout from "./components/Layout";
 import store from "./features/store";
-
-import Button from "./components/Button";
-import Navbar from "./components/navbar";
-import Table from "./components/Table";
-import Auth from "./components/Auth";
 import LoginForm from "./components/LoginForm";
 import SignUp from "./components/SignUp";
+import Landing from "./pages/Landing";
 
 function App() {
   const router = createBrowserRouter([
@@ -25,34 +21,18 @@ function App() {
           path: "signup",
           element: <SignUp />,
         },
+        {
+          path:"home",
+          element: <Landing/>
+        }
       ],
     },
   ]);
 
   return (
-    // <Provider store={store}>
-    //   <RouterProvider router={router}></RouterProvider>
-      
-    // </Provider>
-    <>
-      <Navbar />
-  <div className="container mx-auto p-15 space-x-6">
-    <Button variant="primary">Primary Button</Button>
-    <Button variant="secondary">Secondary Button</Button>
-    <Button variant="success">Success Button</Button>
-    <Button variant="warning">Warning Button</Button>
-    <Button variant="info">Info Button</Button>
-      </div>
-      <Table
-        headers={["Amount", "Item", "Price", "Total"]}
-        data={[
-          [2, "Foul", 6, "12$"],
-          [1, "Ta3meya", 10, "10$"],
-          [3, "Eggs", 5, "15$"],
-          [1, "Cheese", 12, "12$"],
-        ]}
-      />
-    </>
+    <Provider store={store}>
+      <RouterProvider router={router}></RouterProvider>
+    </Provider>
   );
 }
 
---
+++ src/components/Container.jsx
@@ -0,0 +1,3 @@
+export default function Container({ children }) {
+  return <div className="container mx-auto mt-6 px-3">{children}</div>;
+}
